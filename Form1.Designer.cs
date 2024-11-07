13f3d40053e2a89b8239a4709c9d2cc39aa87a0d
ViktorAgafonov <v.v.agafonov@inbox.ru>
2024-11-08 02:16:56 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,473 @@
+﻿namespace EhoMon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            groupBox1 = new GroupBox();
+            label16 = new Label();
+            label11 = new Label();
+            label17 = new Label();
+            txt_time1 = new TextBox();
+            label18 = new Label();
+            label5 = new Label();
+            txt_sernum1 = new TextBox();
+            label4 = new Label();
+            txt_date1 = new TextBox();
+            label3 = new Label();
+            txt_value1 = new TextBox();
+            label2 = new Label();
+            txt_rate1 = new TextBox();
+            label1 = new Label();
+            txt_level1 = new TextBox();
+            checkBox1 = new CheckBox();
+            groupBox2 = new GroupBox();
+            label15 = new Label();
+            label14 = new Label();
+            label13 = new Label();
+            label12 = new Label();
+            txt_date2 = new TextBox();
+            label6 = new Label();
+            txt_sernum2 = new TextBox();
+            label7 = new Label();
+            txt_time2 = new TextBox();
+            label8 = new Label();
+            txt_value2 = new TextBox();
+            label9 = new Label();
+            txt_rate2 = new TextBox();
+            label10 = new Label();
+            txt_level2 = new TextBox();
+            checkBox2 = new CheckBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label16);
+            groupBox1.Controls.Add(label11);
+            groupBox1.Controls.Add(label17);
+            groupBox1.Controls.Add(txt_time1);
+            groupBox1.Controls.Add(label18);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(txt_sernum1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(txt_date1);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(txt_value1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(txt_rate1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(txt_level1);
+            groupBox1.Controls.Add(checkBox1);
+            groupBox1.Location = new Point(10, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(218, 263);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Сток №1 (на территории)";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label16.Location = new Point(157, 124);
+            label16.Name = "label16";
+            label16.Size = new Size(27, 21);
+            label16.TabIndex = 20;
+            label16.Text = "м³";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label11.Location = new Point(5, 192);
+            label11.Name = "label11";
+            label11.Size = new Size(58, 21);
+            label11.TabIndex = 14;
+            label11.Text = "Время:";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label17.Location = new Point(157, 56);
+            label17.Name = "label17";
+            label17.Size = new Size(21, 21);
+            label17.TabIndex = 19;
+            label17.Text = "м";
+            // 
+            // txt_time1
+            // 
+            txt_time1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            txt_time1.Location = new Point(84, 184);
+            txt_time1.Name = "txt_time1";
+            txt_time1.Size = new Size(70, 29);
+            txt_time1.TabIndex = 13;
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label18.Location = new Point(157, 90);
+            label18.Name = "label18";
+            label18.Size = new Size(56, 21);
+            label18.TabIndex = 18;
+            label18.Text = "м³/сек";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label5.Location = new Point(5, 227);
+            label5.Name = "label5";
+            label5.Size = new Size(63, 21);
+            label5.TabIndex = 11;
+            label5.Text = "Зав. №:";
+            // 
+            // txt_sernum1
+            // 
+            txt_sernum1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            txt_sernum1.Location = new Point(84, 218);
+            txt_sernum1.Name = "txt_sernum1";
+            txt_sernum1.Size = new Size(70, 29);
+            txt_sernum1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label4.Location = new Point(5, 158);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 21);
+            label4.TabIndex = 9;
+            label4.Text = "Дата:";
+            // 
+            // txt_date1
+            // 
+            txt_date1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            txt_date1.Location = new Point(84, 150);
+            txt_date1.Name = "txt_date1";
+            txt_date1.Size = new Size(70, 29);
+            txt_date1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label3.Location = new Point(5, 124);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 21);
+            label3.TabIndex = 7;
+            label3.Text = "Объем:";
+            // 
+            // txt_value1
+            // 
+            txt_value1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            txt_value1.Location = new Point(84, 116);
+            txt_value1.Name = "txt_value1";
+            txt_value1.Size = new Size(70, 29);
+            txt_value1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label2.Location = new Point(5, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 21);
+            label2.TabIndex = 5;
+            label2.Text = "Расход:";
+            // 
+            // txt_rate1
+            // 
+            txt_rate1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            txt_rate1.Location = new Point(84, 82);
+            txt_rate1.Name = "txt_rate1";
+            txt_rate1.Size = new Size(70, 29);
+            txt_rate1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(5, 56);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 21);
+            label1.TabIndex = 3;
+            label1.Text = "Уровень:";
+            // 
+            // txt_level1
+            // 
+            txt_level1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            txt_level1.Location = new Point(84, 48);
+            txt_level1.Name = "txt_level1";
+            txt_level1.Size = new Size(70, 29);
+            txt_level1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(19, 23);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(102, 19);
+            checkBox1.TabIndex = 0;
+            checkBox1.Text = "Начать опрос";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label15);
+            groupBox2.Controls.Add(label14);
+            groupBox2.Controls.Add(label13);
+            groupBox2.Controls.Add(label12);
+            groupBox2.Controls.Add(txt_date2);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(txt_sernum2);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Controls.Add(txt_time2);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Controls.Add(txt_value2);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(txt_rate2);
+            groupBox2.Controls.Add(label10);
+            groupBox2.Controls.Add(txt_level2);
+            groupBox2.Controls.Add(checkBox2);
+            groupBox2.Location = new Point(235, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(218, 263);
+            groupBox2.TabIndex = 13;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Сток №2 (за территорией)";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label15.Location = new Point(159, 124);
+            label15.Name = "label15";
+            label15.Size = new Size(27, 21);
+            label15.TabIndex = 17;
+            label15.Text = "м³";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label14.Location = new Point(159, 56);
+            label14.Name = "label14";
+            label14.Size = new Size(21, 21);
+            label14.TabIndex = 16;
+            label14.Text = "м";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label13.Location = new Point(159, 92);
+            label13.Name = "label13";
+            label13.Size = new Size(56, 21);
+            label13.TabIndex = 15;
+            label13.Text = "м³/сек";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label12.Location = new Point(8, 158);
+            label12.Name = "label12";
+            label12.Size = new Size(47, 21);
+            label12.TabIndex = 14;
+            label12.Text = "Дата:";
+            // 
+            // txt_date2
+            // 
+            txt_date2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            txt_date2.Location = new Point(83, 150);
+            txt_date2.Name = "txt_date2";
+            txt_date2.Size = new Size(70, 29);
+            txt_date2.TabIndex = 13;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label6.Location = new Point(8, 227);
+            label6.Name = "label6";
+            label6.Size = new Size(63, 21);
+            label6.TabIndex = 11;
+            label6.Text = "Зав. №:";
+            // 
+            // txt_sernum2
+            // 
+            txt_sernum2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            txt_sernum2.Location = new Point(83, 218);
+            txt_sernum2.Name = "txt_sernum2";
+            txt_sernum2.Size = new Size(70, 29);
+            txt_sernum2.TabIndex = 10;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label7.Location = new Point(7, 192);
+            label7.Name = "label7";
+            label7.Size = new Size(58, 21);
+            label7.TabIndex = 9;
+            label7.Text = "Время:";
+            // 
+            // txt_time2
+            // 
+            txt_time2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            txt_time2.Location = new Point(83, 184);
+            txt_time2.Name = "txt_time2";
+            txt_time2.Size = new Size(70, 29);
+            txt_time2.TabIndex = 8;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label8.Location = new Point(8, 124);
+            label8.Name = "label8";
+            label8.Size = new Size(62, 21);
+            label8.TabIndex = 7;
+            label8.Text = "Объем:";
+            // 
+            // txt_value2
+            // 
+            txt_value2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            txt_value2.Location = new Point(83, 116);
+            txt_value2.Name = "txt_value2";
+            txt_value2.Size = new Size(70, 29);
+            txt_value2.TabIndex = 6;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label9.Location = new Point(8, 90);
+            label9.Name = "label9";
+            label9.Size = new Size(62, 21);
+            label9.TabIndex = 5;
+            label9.Text = "Расход:";
+            // 
+            // txt_rate2
+            // 
+            txt_rate2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            txt_rate2.Location = new Point(83, 82);
+            txt_rate2.Name = "txt_rate2";
+            txt_rate2.Size = new Size(70, 29);
+            txt_rate2.TabIndex = 4;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label10.Location = new Point(4, 56);
+            label10.Name = "label10";
+            label10.Size = new Size(73, 21);
+            label10.TabIndex = 3;
+            label10.Text = "Уровень:";
+            // 
+            // txt_level2
+            // 
+            txt_level2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            txt_level2.Location = new Point(83, 48);
+            txt_level2.Name = "txt_level2";
+            txt_level2.Size = new Size(70, 29);
+            txt_level2.TabIndex = 1;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(19, 22);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(102, 19);
+            checkBox2.TabIndex = 0;
+            checkBox2.Text = "Начать опрос";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(464, 282);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "EhoMon v0.2";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private CheckBox checkBox1;
+        private Label label1;
+        private TextBox txt_level1;
+        private Label label5;
+        private TextBox txt_sernum1;
+        private Label label4;
+        private TextBox txt_date1;
+        private Label label3;
+        private TextBox txt_value1;
+        private Label label2;
+        private TextBox txt_rate1;
+        private GroupBox groupBox2;
+        private Label label6;
+        private TextBox txt_sernum2;
+        private Label label7;
+        private TextBox txt_time2;
+        private Label label8;
+        private TextBox txt_value2;
+        private Label label9;
+        private TextBox txt_rate2;
+        private Label label10;
+        private TextBox txt_level2;
+        private CheckBox checkBox2;
+        private Label label11;
+        private TextBox txt_time1;
+        private Label label12;
+        private TextBox txt_date2;
+        private Label label13;
+        private Label label15;
+        private Label label14;
+        private Label label16;
+        private Label label17;
+        private Label label18;
+    }
+}
